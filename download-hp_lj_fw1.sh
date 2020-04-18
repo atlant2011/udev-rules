a93367210e4292bfdac9c59e49747b7139279fd2
Atlant2011 <4iter-yes@rambler.ru>
2020-04-18 21:31:00 +0700

    Add rules autoloading firmware to winprinter HP LJ 1000,1005,1018,1020 And file download firmware with "foo2zjs" download-hp_lj_fw1.sh

---
+++ download-hp_lj_fw1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+printers="1000 1005 1018 1020 P1005 P1006 P1007 P1008 P1505 P1505"
+cd /tmp
+for prn in $printers; do
+    img="sihp${prn}.img"
+    dl="sihp${prn}.dl"
+    getweb $prn
+    if [ -f $img ]; then
+        arm2hpdl $img > $dl
+#        sudo cp $dl /usr/share/foo2zjs/firmware/$dl
+#        rm $img $dl
+    fi;
+done
